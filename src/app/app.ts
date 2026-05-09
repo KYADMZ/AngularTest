5fb924fba8df595e3783bd63d11bf18182eb17b1
KYADMZ <DMz210@outlook.com>
2026-05-08 22:26:02 -0600

    added function to the goods menu

--- src/app/app.ts
+++ src/app/app.ts
@@ -13,26 +13,36 @@ import { CommonModule } from '@angular/common';
 export class App {
   protected readonly title = signal('AngularTest');
   selectedIndex = -1;
+  //Booleans for the menu options
   showDialog = false;
   showEquipMenu = false;
   check = false;
   dialogText = '';
   showMenuOption = false;
-  items = ['Talk', 'Goods', 'PSI', 'Equip', 'Check'];
+  showGoodsMenu = false;
+
+  //Arrays
+  items = ['Talk', 'Goods', 'PSI', 'Equip', 'Check']; //Main menu options
   backgrounds = [
   { name: 'Straw hat', url: 'url("/assets/pikminback.jpg")' },
   { name: 'Old-worn boots', url: 'url("/assets/earthback.jpg")' },
   { name: 'Sci-fi glasses', url: 'url("/assets/metroidd.gif")' },
   { name: 'Summer shirt', url: 'url("/assets/wuhu.jpg")' },
   { name: 'Nostalgic sweater', url: 'url("/assets/motherback.jpg")' }
-  ];
+  ]; //Background/Equip options
   goods = [
     { good: 'saturn', url:'url("/assets/saturn-valley.gif")' },
     { good: 'starman', url:'url("/assets/starman.gif")' },
     { good: 'moonside', url:'url("/assets/moonside.gif")' },
     { good: 'saturnboard', url:'url("/assets/saturnboard.gif")' },
     { good: 'mother3', url:'url("/assets/mother3.gif")' }
-  ]
+  ] //Check options
+menus = [
+  { name: 'Potion', icon: '/icons/potion.png', action: 'heal' },
+  { name: 'Key', icon: '/icons/key.png', action: 'key' },
+  { name: 'Map', icon: '/icons/map.png', action: 'map' },
+  { name: 'Shield', icon: '/icons/shield.png', action: 'shield' }
+]; //Goods menu options
 
   currentBackground = this.backgrounds[0].url;
   boxitem = this.goods[0].url;
@@ -41,11 +51,14 @@ export class App {
   setSelected(index: number) { this.selectedIndex = index; }
   showMenu() { this.showMenuOption = true; }
   
-  //Code to select the "Talk" option
+  //Code section to select the "Talk" option
   onSelect(item: string) {
     if (item === 'Talk') {
       this.dialogText = 'Who are you talking to?';
       this.showDialog = true;
+    }
+    if (item === 'Goods') {
+      this.showGoodsMenu = true;
     }
     if (item === 'Check') {
       this.check = true;
@@ -56,6 +69,7 @@ export class App {
     
   }
 
+  //Code section for the "Equip" and "Check" options
   selectBackground(bg: any) {
   this.currentBackground = bg.url;
   this.showEquipMenu = false;
@@ -78,5 +92,30 @@ export class App {
     }
   }
 
+  selectItem(item: any) {
   
+  this.handleItemAction(item);
+}
+//Code section for the "Goods" menu options
+  handleItemAction(menu: any) {
+    switch (menu.action) {
+      case 'heal':
+        this.dialogText = 'You used a Potion!  When I was 12 years old, I managed to defeat the final boss of Majoras Mask without the Fierce deity mask with only a quarter heart left. Me and my friend were really hyped!.';
+        break;
+
+      case 'key':
+      this.dialogText = 'The key opens the door! or a coffer?...  For some reason I didnt get any keys to my house until I was 19 years old, so whenever I came back from high-school and nobody was home, I had to climb the house railing and the backyard door to get in. I sometimes got some scratches on my arms.';
+        break;
+
+      case 'map':
+        this.dialogText = 'You check the map.  I played Black-Myth Wukong from release for a good while and I never figured out the button to open the map so I just assumed it was a game feature for immersion or something so it didnt had a map, it wasnt until I got to chapter 5 after around 28 hours of gameplay when I accidentally clicked a button and the map opened! I felt really stupid.';
+        break;
+
+      case 'shield':
+        this.dialogText = 'You equip the Shield!  When I started playing my favorite MMORPG game, I refused to unlock or even level up the tank job that had a shield (dont remember why, I just disliked it so much) so I never touched the job at all. Little did I know that later in the game it would become my favorite job for progging and clearing the most difficult content of the game! I was so harsh with you PLD job, my beloved.';
+        break;
+    }
+
+    this.showDialog = true;
+  }
 }
